e57ec6dc5acfa81f0dc3fafce2b3966dacad641c
Laurent Edel <laurent.edel@gmail.com>
2019-10-01 11:20:45 +0200

    Update to_druid.sql

--- ddl/to_druid.sql
+++ ddl/to_druid.sql
@@ -1,19 +1,19 @@
 --set hive.druid.metadata.username=druid;
 --set hive.druid.metadata.password=StrongPassword;
 --set hive.druid.metadata.uri=jdbc:mysql://mla-hdp26-0.field.hortonworks.com/druid;
-set hive.druid.indexer.partition.size.max=1000000;
-set hive.druid.indexer.memory.rownum.max=100000;
+SET hive.druid.indexer.partition.size.max=1000000;
+SET hive.druid.indexer.memory.rownum.max=100000;
 --set hive.druid.broker.address.default=mla-hdp26-1.field.hortonworks.com:8082;
 --set hive.druid.coordinator.address.default=mla-hdp26-2.field.hortonworks.com:8081;
 --set hive.druid.storage.storageDirectory=/apps/druid/warehouse;
-set hive.tez.container.size=2048;
-set hive.druid.passiveWaitTimeMs=180000;
+SET hive.tez.container.size=2048;
+SET hive.druid.passiveWaitTimeMs=180000;
 
 
-CREATE TABLE trips_druid
+CREATE EXTERNAL TABLE trips_druid
 STORED BY 'org.apache.hadoop.hive.druid.DruidStorageHandler'
 TBLPROPERTIES ( "druid.segment.granularity" = "HOUR",
-  "druid.query.granularity" = "SECOND" )
+  "druid.query.granularity" = "SECOND" )  
 AS
 SELECT 
   npickup_datetime as `__time`,
@@ -34,5 +34,5 @@ SELECT
   tolls_amount, 
   total_amount,
   minute(dropoff_datetime-npickup_datetime) as trip_time
-FROM trips;
-
+FROM trips
+WHERE npickup_datetime IS NOT NULL;
