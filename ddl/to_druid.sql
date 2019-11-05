62779691548b1ec6f6f9a1297bbb60b670048689
Laurent Edel <laurent.edel@gmail.com>
2019-11-05 15:11:18 +0100

    Update to_druid.sql

--- ddl/to_druid.sql
+++ ddl/to_druid.sql
@@ -1,21 +1,15 @@
---set hive.druid.metadata.username=druid;
---set hive.druid.metadata.password=StrongPassword;
---set hive.druid.metadata.uri=jdbc:mysql://mla-hdp26-0.field.hortonworks.com/druid;
 SET hive.druid.indexer.partition.size.max=1000000;
 SET hive.druid.indexer.memory.rownum.max=100000;
---set hive.druid.broker.address.default=mla-hdp26-1.field.hortonworks.com:8082;
---set hive.druid.coordinator.address.default=mla-hdp26-2.field.hortonworks.com:8081;
---set hive.druid.storage.storageDirectory=/apps/druid/warehouse;
 SET hive.tez.container.size=2048;
 SET hive.druid.passiveWaitTimeMs=180000;
 
-
+USE ny_taxi;
 CREATE EXTERNAL TABLE trips_druid
 STORED BY 'org.apache.hadoop.hive.druid.DruidStorageHandler'
-TBLPROPERTIES ( "druid.segment.granularity" = "HOUR",
-  "druid.query.granularity" = "SECOND" )  
+TBLPROPERTIES ( "druid.segment.granularity" = "WEEK",
+  "druid.query.granularity" = "NONE" )
 AS
-SELECT 
+SELECT
   npickup_datetime as `__time`,
   yearmonth,
   cast(`year` as STRING) as `year`,
@@ -28,11 +22,44 @@ SELECT
   cast(SECOND(npickup_datetime) as STRING) as `second`,
   payment_type,
   fare_amount,
-  surcharge, 
-  mta_tax, 
-  tip_amount, 
-  tolls_amount, 
+  surcharge,
+  mta_tax,
+  tip_amount,
+  tolls_amount,
   total_amount,
-  minute(dropoff_datetime-npickup_datetime) as trip_time
+  minute(dropoff_datetime-npickup_datetime)) as trip_time,
+  cast(trips.pickup_longitude as STRING) as longitude,
+  cast(trips.pickup_latitude as STRING) as latitude
 FROM trips
 WHERE npickup_datetime IS NOT NULL;
+
+-- CREATE AGGREGATED TABLE
+-- SO WE GOT TO HAVE NB_TRIPS IN IT TO DISPLAY TOTAL TRIPS
+CREATE EXTERNAL TABLE trips_druid2
+STORED BY 'org.apache.hadoop.hive.druid.DruidStorageHandler'
+TBLPROPERTIES ( "druid.segment.granularity" = "MONTH",
+  "druid.query.granularity" = "MINUTE" )
+AS
+SELECT
+  npickup_datetime as `__time`,
+  yearmonth,
+  cast(`year` as STRING) as `year`,
+  cast(`month` as STRING) as `month`,
+  cast( DayofMonth as STRING ) as DayofMonth,
+  date_format(npickup_datetime,'EEEEE') as `dayOfWeek`,
+  cast(weekofyear(npickup_datetime) as STRING) as `weekofyear`,
+  cast(HOUR(npickup_datetime) as STRING) as `hour`,
+  cast(MINUTE(npickup_datetime) as STRING) as `minute`,
+  cast(SECOND(npickup_datetime) as STRING) as `second`,
+  payment_type,
+  sum(fare_amount) as fare_amount,
+  sum(surcharge) as surcharge,
+  sum(mta_tax) as mta_tax,
+  sum(tip_amount) as tip_amount,
+  sum(tolls_amount) as tolls_amount,
+  sum(total_amount) as total_amount,
+  sum(minute(dropoff_datetime-npickup_datetime)) as trip_time,
+  sum(1) as nb_trips
+FROM trips
+WHERE npickup_datetime IS NOT NULL
+GROUP BY npickup_datetime,yearmonth,year,month,DayofMonth,weekofyear,payment_type ;
